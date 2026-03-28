0880614cddf9b612b5808329b5bfa9c85ece67f3
w4lb3r-c45tr0 <wcastros4@miumg.edu.gt>
2026-03-27 22:49:26 -0600

    Primera prueba

---
+++ src/Encapsulacion/Persona.java
@@ -0,0 +1,25 @@
+package Encapsulacion;
+
+	public class Persona {
+		String nombre;
+		String apellido; 
+		int edad;
+		
+		public Persona(String nombre, String apellido, int edad) {
+			super();
+			this.nombre = nombre;
+			this.apellido = apellido;
+			this.edad = edad;
+			
+		}
+   public void mostrar() {
+   System.out.print("Nombre: "+ nombre + "Apellido: " + apellido + "Edad:" + edad );
+			
+		}
+			
+		}
+
+		
+	
+
+
